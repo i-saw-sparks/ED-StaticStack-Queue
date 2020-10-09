1d779ddfe3edd2ef49b8d7ad41b3e3204997e7f8
i-saw-sparks <andresmolinaceti@gmail.com>
2020-10-08 20:45:24 -0500

    Version entregable

--- Menu.cpp
+++ Menu.cpp
@@ -6,14 +6,20 @@
 
 
 void Menu::run(std::ostream& os, std::istream& is) {
-    Queue<char> infixQueue;
-    std::string infixEx;
-    os<<"Infix to postfix"<<std::endl<<std::endl<<"Type your infix expression: ";
-    is>>infixEx;
+    bool running = true;
+    do {
+        Queue<char> infixQueue;
+        std::string infixEx;
+        os << "Infix to postfix" << std::endl << std::endl << "Type your infix expression: ";
+        is >> infixEx;
 
-    for (char const &c: infixEx)
-       infixQueue.enqueue(c);
+        for (char const &c: infixEx)
+            infixQueue.enqueue(c);
 
-    os<<std::endl;
-    PostfixExp::printAsPostfix(infixQueue, os);
+        os << std::endl<<"Postfix epression: ";
+        PostfixExp::printAsPostfix(infixQueue, os);
+
+        os<<std::endl<<"Do you want to convert another expression? 1-Yes, 2-No"<<std::endl;
+        is>>running;
+    }while(running);
 }
